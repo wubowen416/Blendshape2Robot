a9ad6e1f3fb418a5d0bb1990bb914fd763b3f014
liu <cr-liu@users.noreply.github.com>
2023-02-08 13:48:32 +0900

    Initial Commit

---
+++ Blendshape2Robot/Blendshape2RobotApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blendshape2RobotApp.swift
+//  Blendshape2Robot
+//
+//  Created by liu on 2023/02/08.
+//
+
+import SwiftUI
+
+@main
+struct Blendshape2RobotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
